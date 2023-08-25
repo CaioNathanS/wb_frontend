af7ac88c82f0223dbc6ccad020c6d4fa32d5f879
CaioNathanS <nathansallenave@gmail.com>
2023-08-25 19:45:20 -0300

    feat: cria arquivo de theme da aplicação

---
+++ src/theme/theme.tsx
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+
+
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main:'#99FF99',
+    },
+    secondary: {
+      main:'#ccc',
+    },
+  },
+});
+
+export {defaultTheme};
