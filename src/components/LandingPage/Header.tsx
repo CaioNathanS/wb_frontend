dd6811ef51cbdbe2210e66f6b834c5bde85747c5
CaioNathanS <nathansallenave@gmail.com>
2023-08-25 19:46:10 -0300

    feat: implementa conteúdo inicial do header

--- src/components/LandingPage/Header.tsx
+++ src/components/LandingPage/Header.tsx
@@ -1,7 +1,47 @@
-import { Typography } from "@mui/material";
+import { AppBar, Button, Link, Toolbar, Typography } from "@mui/material";
 
 export function Header (){
-    return <div>
-        <Typography> Landing Page </Typography>
-    </div>
+    return <AppBar
+    position="static"
+    color="default"
+    elevation={0}
+    sx={{ borderBottom: `1px solid  #333` }}
+  >
+
+   <Toolbar sx={{ flexWrap: 'wrap' }}>
+      <Typography variant="h6" color="text" noWrap sx={{ flexGrow: 1 }}>
+        WB Advocacia
+      </Typography>
+      <nav>
+        <Link
+          variant="button"
+          color="text.primary"
+          href="#"
+          sx={{ my: 1, mx: 1.5 }}
+        >
+          Espaço
+        </Link>
+        <Link
+          variant="button"
+          color="text.primary"
+          href="#"
+          sx={{ my: 1, mx: 1.5 }}
+        >
+          Contato
+        </Link>
+        <Link
+          variant="button"
+          color="text.primary"
+          href="#"
+          sx={{ my: 1, mx: 1.5 }}
+        >
+          Novidades
+        </Link>
+      </nav>
+      <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+        Login
+      </Button>
+    </Toolbar>
+
+    </AppBar>
 }
